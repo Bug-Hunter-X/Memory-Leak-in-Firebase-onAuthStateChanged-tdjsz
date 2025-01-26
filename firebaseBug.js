9b71e5c3f804e02e45d3c330f1f50d7dc9128a3f
BugHunter-X <askzenai@gmail.com>
2025-01-26 21:34:06 +0100

    Add firebaseBug.js file

---
+++ firebaseBug.js
@@ -0,0 +1,14 @@
+const unsubscribe = onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid;
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
+
+// unsubscribe is crucial to avoid memory leaks, but it is often forgotten to be called when the component unmounts
+unsubscribe(); // this line is often missed
